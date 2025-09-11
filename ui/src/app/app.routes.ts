6b302828c24104e4c60f95eb98cf49077ea54753
evangelospap <evangelakion.13@gmail.com>
2025-09-12 02:53:09 +0300

    Logo + home Page url small refactor + routing

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -6,7 +6,10 @@ import { CountryStatsComponent } from './components/country-stats/country-stats.
 import { CountryDataComponent } from './components/country-data/country-data.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: HomeComponent }, // Home is the default
+  // Redirect empty path to 'home'
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // The actual HomeComponent mapped to 'home'
+  { path: 'home', component: HomeComponent },
   { path: 'countries', component: CountriesComponent },
   { path: 'countries/:id/languages', component: CountryLanguagesComponent },
   { path: 'countries/stats', component: CountryStatsComponent },
